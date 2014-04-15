c1cc6387791fd1d3019507f827d96ffa0f981251
Konstantin Ilchenko <konstantin@ilchenko.by>
2014-04-15 10:15:08 +0300

    exclude test folder from coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,13 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'test'
+end
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
